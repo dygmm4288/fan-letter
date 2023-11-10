f2b86d214fae0adb9f43c2e3ace713bf4bab209b
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-10 22:39:39 +0900

    CSS 적용하기 완료

--- src/components/LetterDetailWrapper.jsx
+++ src/components/LetterDetailWrapper.jsx
@@ -3,6 +3,7 @@ import { alter } from "lib/alter";
 import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import Avatar from "./Avatar";
 import Button from "./Button";
 
 export default function LetterDetailWrapper({ letters, setMemberLetterList }) {
@@ -61,6 +62,7 @@ const LetterDetail = ({
     );
   };
   const handleRemoveButton = () => {
+    if (!window.confirm("정말로 삭제하시겠습니까?")) return;
     setMemberLetterList((prev) => prev.filter((letter) => letter.id !== id));
     navigate("/");
   };
@@ -81,8 +83,8 @@ const LetterDetail = ({
   return (
     <StyledDetail>
       <StyledDetailHeader>
-        <img src={avatar} alt={nickname + "profile image"} />
-        <span>{nickname}</span>
+        <Avatar nickname={nickname} src={avatar} />
+        <h2>{nickname}</h2>
         <span>{createdAt}</span>
       </StyledDetailHeader>
       <span>To : {memberKoreanMap[writedTo]}</span>
@@ -94,13 +96,62 @@ const LetterDetail = ({
           value={contentValue}
         />
       )}
-      {!isUpdate ? <UpdateButton /> : <UpdateDoneButton />}
-      <RemoveButton />
+      <StyledButtonWrapper>
+        {!isUpdate ? <UpdateButton /> : <UpdateDoneButton />}
+        <RemoveButton />
+      </StyledButtonWrapper>
     </StyledDetail>
   );
 };
 
-const StyledDetail = styled.div``;
-const StyledDetailHeader = styled.div``;
-const StyledContent = styled.p``;
-const StyledContentTextArea = styled.textarea``;
+const StyledDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  color: white;
+  width: 800px;
+  min-height: 500px;
+  background-color: gray;
+  padding: 1rem;
+
+  & > span {
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+`;
+const StyledDetailHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1rem;
+
+  h2 {
+    flex: 1;
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+`;
+const StyledContent = styled.p`
+  background-color: black;
+  min-height: 250px;
+  border-radius: 0.5rem;
+  padding: 2rem 1rem;
+  color: white;
+`;
+const StyledContentTextArea = styled.textarea`
+  background-color: black;
+  min-height: 250px;
+  border-radius: 0.5rem;
+  padding: 2rem 1rem;
+  color: white;
+`;
+
+const StyledButtonWrapper = styled.div`
+  align-self: flex-end;
+  display: flex;
+  gap: 0.5rem;
+  * {
+    font-size: 1.5rem;
+  }
+`;
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,17 +1,46 @@
 import LetterDetailWrapper from "components/LetterDetailWrapper";
 import { Link, useParams } from "react-router-dom";
+import styled from "styled-components";
 
 export default function Detail({ memberLetterList, setMemberLetterList }) {
   const { member, id } = useParams();
   const findLetterById = () =>
     memberLetterList[member].find((letter) => letter.id === id);
   return (
-    <div>
+    <StyledDetail>
       <Link to='/'> 홈으로</Link>
       <LetterDetailWrapper
         letters={findLetterById()}
         setMemberLetterList={setMemberLetterList}
       />
-    </div>
+    </StyledDetail>
   );
 }
+
+const StyledDetail = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  & > a {
+    padding: 1rem 1.5rem;
+    background-color: black;
+    color: white;
+    display: inline-block;
+    text-decoration: none;
+    font-weight: 500;
+    font-size: 0.9rem;
+
+    border-left: 1.5px solid gray;
+    border-top: 1.5px solid gray;
+
+    box-shadow: 5px 5px 5px black;
+
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+  }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+`;
