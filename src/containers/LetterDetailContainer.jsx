a5e1935a69cf6d7d6ebe1b2bf56b6786d0245eaa
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-17 15:47:37 +0900

    모달 창 HOC 패턴으로 구현

--- src/containers/LetterDetailContainer.jsx
+++ src/containers/LetterDetailContainer.jsx
@@ -1,3 +1,5 @@
+import AlertModal from "components/AlertModal";
+import ConfirmModal from "components/ConfirmModal";
 import EmptyLetterDetail from "components/EmptyLetterDetail";
 import { alter } from "lib/alter";
 import {
@@ -6,6 +8,13 @@ import {
   selectMemberLetterList,
   updateMemberLetter,
 } from "modules/memberLetters";
+import {
+  IS_ALERT,
+  selectIsAlert,
+  selectIsConfirm,
+  setModalAllClose,
+  setModalState,
+} from "modules/modal";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
@@ -17,38 +26,49 @@ export default function LetterDetailContainer() {
   const letter = memberLetterList[member].find(findLetterById(id));
   const [isUpdate, setIsUpdate] = useState(false);
   const [contentValue, setContentValue] = useState(letter.content);
+  const isConfirm = useSelector(selectIsConfirm);
+  const isAlert = useSelector(selectIsAlert);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const handleUpdateButton = () => setIsUpdate(true);
+
   const handleUpdateDoneButton = () => {
     setIsUpdate(false);
     if (contentValue === letter.content) {
-      alert("아무런 수정 사항이 없습니다.");
+      // alert("아무런 수정 사항이 없습니다.");
+      dispatch(setModalState({ key: IS_ALERT, value: true }));
       return;
     }
     dispatch(updateMemberLetter({ id, content: contentValue }));
   };
   const handleRemoveButton = () => {
-    if (!window.confirm("정말로 삭제하시겠습니까?")) return;
     dispatch(deleteMemberLetter({ id }));
+    dispatch(setModalAllClose());
     navigate("/");
   };
+
   const handleChangeContent = (e) => {
     setContentValue(e.target.value);
   };
   const ifEmptyLetterThan = alter(() => !letter);
-  return ifEmptyLetterThan(
-    <EmptyLetterDetail />,
-    <LetterDetail
-      letter={letter}
-      handleChangeContent={handleChangeContent}
-      isUpdate={isUpdate}
-      contentValue={contentValue}
-      handleRemoveButton={handleRemoveButton}
-      handleUpdateButton={handleUpdateButton}
-      handleUpdateDoneButton={handleUpdateDoneButton}
-    />,
+
+  return (
+    <>
+      {ifEmptyLetterThan(
+        <EmptyLetterDetail />,
+        <LetterDetail
+          letter={letter}
+          handleChangeContent={handleChangeContent}
+          isUpdate={isUpdate}
+          contentValue={contentValue}
+          handleUpdateButton={handleUpdateButton}
+          handleUpdateDoneButton={handleUpdateDoneButton}
+        />,
+      )}
+      {isConfirm && <ConfirmModal handleConfirm={handleRemoveButton} />}
+      {isAlert && <AlertModal text='아무런 수정 사항이 없습니다.' />}
+    </>
   );
 }
